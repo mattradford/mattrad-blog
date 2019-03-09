935904aa2e6f0e4eadca0b66301e5423a67f1b9c
Matt Radford <webform@mattrad.co.uk>
2019-03-09 08:31:21 +0000

    Date changes

--- .eleventy.js
+++ .eleventy.js
@@ -12,10 +12,16 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("readableDate", dateObj => {
     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
   });
+  eleventyConfig.addFilter("shortDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL");
+  });
 
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter('htmlDateString', (dateObj) => {
     return DateTime.fromJSDate(dateObj).toFormat('yyyy-LL-dd');
+  });
+  eleventyConfig.addFilter('htmlYearString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toFormat('yyyy');
   });
 
   // Get the first `n` elements of a collection.
--- posts/tooltips-gravity-forms.md
+++ posts/tooltips-gravity-forms.md
@@ -2,6 +2,5 @@
 title: Tooltips for Gravity Forms
 date: 2014-04-30
 excerpt: And that’s the power of extensible WordPress. A client request, a quick Google, and there is a solution. In this case, tootips for Gravity Forms.
-permalink: https://github.com/norcross/gravity-tooltips
-layout: layouts/post.njk
+permalink: "https://github.com/norcross/gravity-tooltips"
 ---
